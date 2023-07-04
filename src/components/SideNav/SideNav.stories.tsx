92fd5a5ce6022bbbe8e1eabe0470ef405e2e7040
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2023-07-04 18:10:09 +0530

    Add SideNav component

---
+++ src/components/SideNav/SideNav.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StoryFn, Meta, StoryObj } from "@storybook/react";
+import { MdContacts } from "react-icons/md";
+import { IoMdContacts } from "react-icons/io";
+import { PiContactlessPaymentFill } from "react-icons/pi";
+
+import SideNav, { SideNavItem, SideNavList } from "./SideNav";
+
+export default {
+  title: "Components/SideNav",
+  component: SideNav,
+} as Meta<typeof SideNav>;
+
+const Template: StoryFn<typeof SideNav> = (args: any) => <SideNav {...args} />;
+
+type Story = StoryObj<typeof Template>;
+
+export const Default: Story = {
+  render: (args: any) => (
+    <SideNav {...args}>
+      <SideNavList>
+        <SideNavItem icon={<MdContacts />}>Item 1</SideNavItem>
+        <SideNavItem icon={<IoMdContacts />} active>
+          Item 2 item 2
+        </SideNavItem>
+        <SideNavItem icon={<PiContactlessPaymentFill />}>Item 3</SideNavItem>
+      </SideNavList>
+    </SideNav>
+  ),
+};
---
+++ src/components/SideNav/SideNav.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+
+import { cn } from "../../lib/utils";
+import { cva } from "class-variance-authority";
+
+export interface SideNavProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const SideNav = React.forwardRef<HTMLDivElement, SideNavProps>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <aside
+        {...props}
+        ref={ref}
+        className={cn("p-8 bg-white w-max h-full shadow-md", className)}
+      >
+        {children}
+      </aside>
+    );
+  }
+);
+SideNav.displayName = "SideNav";
+
+const SideNavList = React.forwardRef<
+  HTMLUListElement,
+  React.HTMLAttributes<HTMLUListElement>
+>(({ children, className, ...props }, ref) => {
+  return (
+    <ul {...props} ref={ref} className={cn("space-y-4", className)}>
+      {children}
+    </ul>
+  );
+});
+SideNavList.displayName = "SideNavList";
+
+const sideNavItemVariants = cva(
+  "p-5 py-3 rounded-lg text-grey-900 cursor-pointer flex items-center space-x-4 whitespace-nowrap font-semibold active:bg-primary-50 active:text-primary-500",
+  {
+    variants: {
+      active: {
+        true: "bg-primary-50 text-primary-500",
+        false: "hover:bg-grey-100",
+      },
+    },
+    defaultVariants: {
+      active: false,
+    },
+  }
+);
+
+const SideNavItem = React.forwardRef<
+  HTMLLIElement,
+  {
+    icon?: React.ReactNode;
+    active?: boolean;
+  } & React.HTMLAttributes<HTMLLIElement>
+>(({ children, className, icon, active, ...props }, ref) => {
+  return (
+    <li
+      {...props}
+      ref={ref}
+      className={cn(sideNavItemVariants({ active }), className)}
+    >
+      {icon && <span className="text-2xl">{icon}</span>}
+      <span>{children}</span>
+    </li>
+  );
+});
+SideNavItem.displayName = "SideNavItem";
+
+export { SideNavList, SideNavItem };
+export default SideNav;
