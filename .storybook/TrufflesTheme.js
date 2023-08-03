d308844b5dc02ed2be09a8bceda1143d83fa3202
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2023-08-03 12:41:24 +0530

    customize storybook theme

---
+++ .storybook/TrufflesTheme.js
@@ -0,0 +1,16 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  brandTitle: "Truffles",
+  brandUrl: "https://truffles.one",
+  brandImage: "https://app.truffles.one/logo.svg",
+  brandTarget: "_self",
+
+  fontBase: '"Inter", sans-serif',
+  fontCode: "monospace",
+
+  colorPrimary: "#B4B0C1",
+  colorSecondary: "#7445F8",
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from "@storybook/manager-api";
+
+import TrufflesTheme from "./TrufflesTheme";
+
+addons.setConfig({
+  theme: TrufflesTheme,
+});
